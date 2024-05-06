07b11eaec4804d9a270c7a209a0b3c3857b84983
ddealsan <ddealsan@myuax.com>
2024-05-06 14:18:31 +0200

    avance

--- src/main/java/AlmacenamientoPoblaciones/ExperimentoWriter.java
+++ src/main/java/AlmacenamientoPoblaciones/ExperimentoWriter.java
@@ -7,14 +7,15 @@ import java.io.ObjectOutputStream;
 import java.util.List;
 
 public class ExperimentoWriter {
-
-    public static void escribirExperimento(String archivo, Experimento experimento){
+    public void escribirExperimento(Experimento experimento, String filename) {
         try {
-            FileOutputStream fileOut = new FileOutputStream(archivo + experimento.getNombre() + ".txt", true);
-            ObjectOutputStream escritor = new ObjectOutputStream(fileOut);
-            escritor.writeObject(experimento);
-        } catch (Exception e) {
-            e.printStackTrace();
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(experimento);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
         }
     }
 }
